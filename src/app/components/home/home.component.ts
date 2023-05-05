ce026c925abd13f3d6e6834b0a61feabd300aa68
Jordi <jordilop@gmail.com>
2023-05-05 12:48:33 +0200

    Exercici 9 with erros

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { DatePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TotalAmountServiceService } from 'src/app/total-amount-service.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   totalAmount = 0;
   totalPrices = [0, 0, 0];
   prices = [500, 300, 200];
@@ -19,7 +20,54 @@ export class HomeComponent {
 
   pipe = new DatePipe('es-ES');
 
-  constructor(private totalAmountService: TotalAmountServiceService) { }
+  queryParams = {
+    web: false,
+    seo: false,
+    ads: false,
+    pages: this.webServices[0],
+    langs: this.webServices[1]
+  };
+
+  constructor(private totalAmountService: TotalAmountServiceService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // this.router.navigate([], {
+    //   // relativeTo: this.route,
+    //   queryParams: {
+    //     web: this.totalPrices[0] != 0 ? true : false,
+    //     seo: this.totalPrices[1] != 0 ? true : false,
+    //     ads: this.totalPrices[2] != 0 ? true : false,
+    //     pages: this.webServices[0],
+    //     langs: this.webServices[1]
+    //   },
+    //   queryParamsHandling: 'merge',
+    // })
+    // this.setQueryParams();
+    this.getQueryParams();
+    // this.setQueryParams();
+  }
+
+  getQueryParams() {
+    this.route.queryParams.subscribe(params => {
+      console.log("GET! " + Boolean(params['web']));
+      console.log(this.totalPrices);
+      Boolean(params['web']) ? this.totalPrices[0] = this.prices[0] : false;
+      console.log(this.totalPrices);
+    })
+  }
+
+  setQueryParams() {
+    this.router.navigate([], {
+      queryParams: {
+        web: this.totalPrices[0] != 0 ? true : false,
+        seo: this.totalPrices[1] != 0 ? true : false,
+        ads: this.totalPrices[2] != 0 ? true : false,
+        pages: this.webServices[0],
+        langs: this.webServices[1]
+      }
+    })
+    console.log("SET!!")
+  }
 
   onChanged(e: any) {
     let { name, checked } = e.target;
@@ -36,16 +84,20 @@ export class HomeComponent {
     checked ? this.totalPrices[index] = this.prices[index] : this.totalPrices[index] = 0;
 
     this.totalAmount = this.totalAmountService.calculateTotalAmount(this.totalPrices, this.webServices);
+
+    this.setQueryParams();
   }
 
   receivePages(e: any) {
     this.webServices[0] = Number(e);
     this.totalAmount = this.totalAmountService.calculateTotalAmount(this.totalPrices, this.webServices);
+    this.setQueryParams();
   }
 
   receiveLangs(e: any) {
     this.webServices[1] = Number(e);
     this.totalAmount = this.totalAmountService.calculateTotalAmount(this.totalPrices, this.webServices);
+    this.setQueryParams();
   }
 
   setQuotation() {
